a94c1f43de8335e55826f7d13b9aae02693d9425
Xai Hang <xaihang12@gmail.com>
2023-01-21 15:34:15 -0600

    added employee obj and setup html form

--- script/script.js
+++ script/script.js
@@ -1,5 +1,30 @@
 $(document).ready(onReady);
 
+// STATE
+let employees = [
+    {
+        firstName: 'Jen',
+        lastName: 'Barber',
+        idNumber: 4521,
+        jobTitle: 'Team Lead',
+        annualSalary: 80000
+    },
+    {
+        firstName: 'Maurice',
+        lastName: 'Moss',
+        idNumber: 8724,
+        jobTitle: 'Support Team',
+        annualSalary: 58000
+    },
+    {
+        firstName: 'Roy',
+        lastName: 'Smith',
+        idNumber: 9623,
+        jobTitle: 'Quality Assurance',
+        annualSalary: 48000
+    }
+];
+
 function onReady() {
     console.log('document is ready!');
 }
