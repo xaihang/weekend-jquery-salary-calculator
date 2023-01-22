bc0fbd412e824bde2fb5db54343218d7d6e21cc3
Xai Hang <xaihang12@gmail.com>
2023-01-21 18:13:54 -0600

    fixed display on dom bug

--- script/script.js
+++ script/script.js
@@ -1,44 +1,34 @@
 $(document).ready(onReady);
 
-// form:
-//          #addEmployeeForm
-    // inputs:
-    //          #firstNameInput  
-    //          #lastNameInput 
-    //          #idNumberInput 
-    //          #jobTitleInput 
-    //          #annualSalaryInput 
-    //          #submit    
-
-// table body <tbody>
-//          #employee-table
-
-
 // STATE
-let employees = [{firstName: 'Jen', lastName: 'Barber', idNumber: 4521, jobTitle: 'Team Lead', annualSalary: 80000},
-    {firstName: 'Maurice', lastName: 'Moss', idNumber: 8724, jobTitle: 'Support Team', annualSalary: 58000},
-    {firstName: 'Roy', lastName: 'Smith', idNumber: 9623, jobTitle: 'Quality Assurance', annualSalary: 48000}];
+let employees = [{firstName: 'Jen', lastName: 'Barber', idNumber: 4521, title: 'Team Lead', annualSalary: 80000},
+    {firstName: 'Maurice', lastName: 'Moss', idNumber: 8724, title: 'Support Team', annualSalary: 58000},
+    {firstName: 'Roy', lastName: 'Smith', idNumber: 9623, title: 'Quality Assurance', annualSalary: 48000}];
 
 function onReady() {
     render();
     // handle displaying the new employee's input on DOM
-    $('#submit').click(submitEmployee);
+    $('#addEmployeeForm').on('submit', onAddEmployee)
     // handle removing employee from DOM
-    $(document).click('.deleteBtn', onDeleteEmployee);
+    $(document).on('click', '.deleteBtn', onDeleteEmployee);
 
-}
+};
 
 function render() {
  
+
+    // empty ()
+    $('#employee-table').empty();
+
  // update the employee's inputs:   
     for (let employee of employees) {
         console.log('for each employee...', employee.firstName);
-        $('#cat-table').append(`
+        $('#employee-table').append(`
         <tr>
             <td>${employee.firstName}</td>
             <td>${employee.lastName}</td>
             <td>${employee.idNumber}</td>
-            <td>${employee.jobTitle}</td>
+            <td>${employee.title}</td>
             <td>${employee.annualSalary}</td>
             <td>
                 <button class="deleteBtn">Delete</button>
@@ -46,26 +36,28 @@ function render() {
         </tr>
     `)
     }
-}
+
+};
 
 // add employee's inputs to the database:
-function submitEmployee(event) {
+function onAddEmployee(event) {
     event.preventDefault(); 
 
     let newEmployee = {
         firstName: $('#firstNameInput').val(), 
         lastName: $('#lastNameInput').val(),
         idNumber: $('#idNumberInput').val(),
-        jobTitleInput: $('#jobTitleInput').val(),
+        jobTitleInput: $('#titleInput').val(),
         annualSalaryInput: $('#annualSalaryInput').val()
     }
-console.log('new employee:', newEmployee);
     employees.push(newEmployee);
     render(); 
-}
+};
 
 function onDeleteEmployee() {
     let currentTableRow= $(this).parent().parent();
     let indexOfEmployee = currentTableRow.index();
-    console.log('indexOfEmployee', indexOfEmployee);
-}
+    employees.splice(indexOfEmployee, 1);
+    // console.log('employee after removal', employees);
+    render(); 
+};
